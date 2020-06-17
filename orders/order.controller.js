940e4af3ce161a75712d110031f575914865d57a
Thân Thái <thaitv210@gmail.com>
2020-06-18 00:49:00 +0700

    cmt

--- orders/order.controller.js
+++ orders/order.controller.js
@@ -1,4 +1,4 @@
-const db = require('../models/database');
+const db = require('../models/dbsqlite');
 const jwt = require('jsonwebtoken');
 const key = 'vietnam';
 const generalErr = require('../messages').generalErr;
@@ -28,29 +28,27 @@ const getOne = function (req, res) {
 };
 
 const create = function (req, res) {
-    let sql = 'INSERT INTO `order` (product_id, quantity, created_at, updated_at) VALUES (?, ?, ?, ?)';
-    let params = [
-        req.body['productId'],
-        req.body['quantity'],
-        new Date(),
-        new Date(),
-    ];
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            console.log(err);
-            generalErr(res);
-            return
-        }
-        let id = result.insertId;
-        let sql = 'SELECT `order`.*, product.properties FROM `order` INNER JOIN product ON `order`.`product_id` = product.id WHERE `order`.`id` = ?';
-        db.query(sql, [id], (err, result) => {
-            if (err) {
-                generalErr(res);
-                return
-            }
-            handleGet(res, result[0])
+    let timestamp = new Date();
+    let data = req.body['data'];
+    let flag = true;
+    data.map(item => {
+        let sql = 'INSERT INTO tblorder (id, product_id, quantity, created_at, updated_at) VALUES (?, ?, ?, ?, ?)'
+        let params = [
+            timestamp.getTime(),
+            item['productId'],
+            item['quantity'],
+            timestamp.getTime(),
+            timestamp.getTime()
+        ];
+        db.run(sql, params, (result) => {
+            if (!result['lastID']) flag = false
         })
-    })
+    });
+    if (flag) {
+        res.status(201).json({message: 'Success'})
+    } else {
+        res.status(400).json({message: 'Failed'})
+    }
 };
 
 const update = function (req, res) {
